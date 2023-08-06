8819b560cd6f24a518be87d9664d497a9b900878
liuminhui <huidbk@163.com>
2023-08-07 01:02:44 +0800

    helloworld-demo

---
+++ 01-HelloWorld/main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author: Hui
+# @Desc: { fastapi hello world 案例 }
+# @Date: 2023/08/06 22:16
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI(description="hello world")
+
+
+@app.get(path="/")
+def index():
+    return "index"
+
+
+@app.get(path="/hello")
+def hello():
+    return "hello world"
+
+
+def main():
+    uvicorn.run(app)
+
+
+if __name__ == '__main__':
+    main()
